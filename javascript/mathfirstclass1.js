5d521dc4faba012ba351561e4880d009c2fb9ce3
Luciano2792 <adono2002@hotmail.com>
2018-07-27 00:57:10 -0500

    Faltan detalles del primer html

--- javascript/mathfirstclass1.js
+++ javascript/mathfirstclass1.js
@@ -1,10 +1,12 @@
 $(".firstbutton").click(function(){
     $(".sumar").append(
-        "<div class='col-md-7'><p><strong> Como operación matemática, la suma consiste en añadir dos números o más para obtener una cantidad total. Este proceso también permite reunir dos grupos de cosas para obtener un único conjunto. Por ejemplo: si tengo tres manzanas y tomo otras dos, tendré cinco manzanas (3+2=5).</strong></p></div><div class='col-md-3'><img src='../Sources/sumanzanas.png' width='90%' height='90%' style='border: 5px solid #C1BF2A; border-radius: 50px;'></div>");
+        "<div class='col-md-7'><p><strong>Miralo con un ejemplo de la vida real. Imagina que tienes 4 manzanas, tu amigo te regala 2. Ahora ¿Cuántas manzanas tienes tú?. Exacto, tendrías 6 (4+2=6), pero ¿Por qué es esto?; resulta que la suma consiste en tomar un número y adicionarle otro número, así sumando sus digitos.</strong></p></div><div class='col-md-3'><img src='../Sources/sumanzanas.png' width='90%' height='90%' style='border: 5px solid #C1BF2A; border-radius: 50px;'></div>");
     $("#secondbutton").prop("hidden", false);
     $(".center").remove(".center");
 });
 
 $("#secondbutton").click(function(){
-    $("#restar").append("<div class='col-md-12'><h2 id='title'>Restar</h2></div>");
+    $("#restar").append("<div class='col-md-12'><h2 id='title'>Restar</h2></div><div class='col-md-7><p><strong>Tomemos el mismo ejemplo de arriba, pero en lugar de que tu amigo te de 2 manzanas, tú le das 2 manzanas a él. Ahora, ¿Cuántas manzanas tienes tú?. Exacto, tendrías sólo 2 (4-2=2), pero ¿Por qué esto?; resulta que la resta consiste en tomar un número y quitarle otro número, es decir, restando el valor del número inicial con el valor </strong></p><div>");
+    $(".2button").remove(".2button");
+    $("#thirdbutton").prop("hidden", false);
 });
