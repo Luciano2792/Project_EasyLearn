e570a0f28dcb2d08bd35fb368bb4158d63691848
Luciano2792 <adono2002@hotmail.com>
2018-07-30 00:59:42 -0500

    Terminado matematicas primaria, revisar carpeta de trash luego

--- javascript/mathfirstclass1.js
+++ javascript/mathfirstclass1.js
@@ -1,12 +1,13 @@
 $(".firstbutton").click(function(){
     $(".sumar").append(
-        "<div class='col-md-7'><p><strong>Miralo con un ejemplo de la vida real. Imagina que tienes 4 manzanas, tu amigo te regala 2. Ahora ¿Cuántas manzanas tienes tú?. Exacto, tendrías 6 (4+2=6), pero ¿Por qué es esto?; resulta que la suma consiste en tomar un número y adicionarle otro número, de forma que se juntan dos números en uno.</strong></p></div><div class='col-md-3'><img src='../Sources/sumanzanas.png' width='90%' height='90%'></div>");
+        "<div class='col-md-7'><p><strong>Miralo con un ejemplo de la vida real. Imagina que tienes 4 manzanas, tu amigo te regala 2. Ahora ¿Cuántas manzanas tienes tú?. Exacto, tendrías 6 (4+2=6), pero ¿Por qué es esto?. Resulta que la suma consiste en tomar un número y adicionarle otro número, de forma que se juntan dos números en uno.</strong></p><p><strong>Cabe destacar que para escribir una suma de forma algebraica se representa con el signo '+'</div><div class='col-md-3'><img src='../Sources/sumanzanas.png' width='90%' height='90%'></div>");
     $("#secondbutton").prop("hidden", false);
+    $("#title").prop("hidden", false);
     $(".center").remove(".center");
 });
 
 $("#secondbutton").click(function(){
-    $("#restar").append("<div class='col-md-12'><h2 id='title'>Restar</h2></div><div class='col-md-7'><p><strong>Tomemos el mismo ejemplo de arriba, pero en lugar de que tu amigo te de 2 manzanas, tú le das 2 manzanas a él. Ahora, ¿Cuántas manzanas tienes tú?. Exacto, tendrías sólo 2 (4-2=2), pero ¿Por qué esto?. Resulta que la resta consiste en tomar un número y quitarle otro número, es decir, quitando una cantidad de otra. </strong></p></div><div class='col-md-3'><img src='../Sources/Ejemplo-de-resta1.jpg' width='90%' height='90%'></div></div>");
+    $("#restar").append("<div class='col-md-12'><h2 id='title'>Restar</h2></div><div class='col-md-7'><p><strong>Tomemos el mismo ejemplo de arriba, pero en lugar de que tu amigo te de 2 manzanas, tú le das 2 manzanas a él. Ahora, ¿Cuántas manzanas tienes tú?. Exacto, tendrías sólo 2 (4-2=2), pero ¿Por qué esto?. Resulta que la resta consiste en tomar un número y quitarle otro número, es decir, quitando una cantidad de otra. Cabe destacar que para escribir una resta de forma algebraica se representa con el signo '-'</strong></p></div><div class='col-md-3'><img src='../Sources/Ejemplo-de-resta1.jpg' width='90%' height='90%'></div></div>");
     $(".question").prop("hidden", false);
     $(".2button").remove(".2button");
     $("#thirdbutton").prop("hidden", false);
--- javascript/mathfirstclass3.js
+++ javascript/mathfirstclass3.js
@@ -2,6 +2,46 @@ jQuery(document).ready(function($){
     $("#bsubmit").click(
         function(e){
             e.preventDefault();
+
+            if($("#firstanswer1")[0].checked == true){
+                $("#alert1").append("<h4 style='color: rgb(221, 6, 6);'><strong> Lo siento, la respuesta ha sido incorrecta. <a href='mathfirstclass1.html'>Repasa los conceptos</a> e intentalo de nuevo</strong></h4>");
+            } else if($("#secondanswer1")[0].checked == true){
+                $("#alert1").append("<h4 style='color: rgb(221, 6, 6);'><strong> Lo siento, la respuesta ha sido incorrecta. <a href='mathfirstclass1.html'>Repasa los conceptos</a> e intentalo de nuevo</strong><h4>");
+            } else if($("#thirdanswer1")[0].checked == true){
+                $("#alert1").append("<h4 style='color: #00BB0E;'><strong>La respuesta es correcta!</strong></h4>");
+            } else if($("#fourthanswer1")[0].checked == true){
+                $("#alert1").append("<h4 style='color: rgb(221, 6, 6);'><strong> Lo siento, la respuesta ha sido incorrecta. <a href='mathfirstclass1.html'>Repasa los conceptos</a> e intentalo de nuevo</strong></h4>");
+            } else{
+                $("#alert1").append("<h4 style='color: rgb(221, 6, 6); text-align: center;'><strong>Porfavor selecciona una respuesta!</strong></h4>");
+           }
+
+           if($("#firstanswer2")[0].checked == true){
+                $("#alert2").append("<h4 style='color: #00BB0E;'><strong>La respuesta es correcta!</strong></h4>");
+            } else if($("#secondanswer2")[0].checked == true){
+                $("#alert2").append("<h4 style='color: rgb(221, 6, 6);'><strong> Lo siento, la respuesta ha sido incorrecta. <a href='mathfirstclass1.html'>Repasa los conceptos</a> e intentalo de nuevo</strong><h4>");
+            } else if($("#thirdanswer2")[0].checked == true){
+                $("#alert2").append("<h4 style='color: rgb(221, 6, 6);'><strong> Lo siento, la respuesta ha sido incorrecta. <a href='mathfirstclass1.html'>Repasa los conceptos</a> e intentalo de nuevo</strong><h4>");
+            } else if($("#fourthanswer2")[0].checked == true){
+                $("#alert2").append("<h4 style='color: rgb(221, 6, 6);'><strong> Lo siento, la respuesta ha sido incorrecta. <a href='mathfirstclass1.html'>Repasa los conceptos</a> e intentalo de nuevo</strong></h4>");
+            } else{
+                $("#alert2").append("<h4 style='color: rgb(221, 6, 6); text-align: center;'><strong>Porfavor selecciona una respuesta!</strong></h4>");
+            }
+            
+            if($("#firstanswer3")[0].checked == true){
+                $("#alert3").append("<h4 style='color: rgb(221, 6, 6);'><strong> Lo siento, la respuesta ha sido incorrecta. <a href='mathfirstclass1.html'>Repasa los conceptos</a> e intentalo de nuevo</strong><h4>");
+            } else if($("#secondanswer3")[0].checked == true){
+                $("#alert3").append("<h4 style='color: rgb(221, 6, 6);'><strong> Lo siento, la respuesta ha sido incorrecta. <a href='mathfirstclass1.html'>Repasa los conceptos</a> e intentalo de nuevo</strong><h4>");
+            } else if($("#thirdanswer3")[0].checked == true){
+                $("#alert3").append("<h4 style='color: rgb(221, 6, 6);'><strong> Lo siento, la respuesta ha sido incorrecta. <a href='mathfirstclass1.html'>Repasa los conceptos</a> e intentalo de nuevo</strong><h4>");
+            } else if($("#fourthanswer3")[0].checked == true){
+                $("#alert3").append("<h4 style='color: #00BB0E;'><strong>La respuesta es correcta!</strong></h4>");
+            } else{
+                $("#alert3").append("<h4 style='color: rgb(221, 6, 6); text-align: center;'><strong>Porfavor selecciona una respuesta!</strong></h4>");
+            }
+
+            if($("#thirdanswer1")[0].checked == true && $("#firstanswer2")[0].checked == true && $("#fourthanswer3")[0].checked == true){
+                location.href="first.html"
+            }
         }
     )
 });
