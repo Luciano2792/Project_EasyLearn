e6bdbd46be625f2b8539cc0658874e45cd690b86
AngieVL <Luciano2792>
2018-08-04 08:39:11 -0500

    ÚLIMO COMMIT. <3

--- javascript/maththirdclass3.js
+++ javascript/maththirdclass3.js
@@ -34,13 +34,13 @@ jQuery(document).ready(function($){
             e.preventDefault();
 
             if($("#firstanswer1")[0].checked == true){
-                $("#alert1").append("<h4 style='color: rgb(221, 6, 6);'><strong> Lo siento, la respuesta ha sido incorrecta. <a href='mathfirstclass1.html'>Repasa los conceptos</a> e intentalo de nuevo</strong></h4>");
+                $("#alert1").append("<h4 style='color: rgb(221, 6, 6);'><strong> Lo siento, la respuesta ha sido incorrecta. <a href='maththirdclass1.html'>Repasa los conceptos</a> e intentalo de nuevo</strong></h4>");
             } else if($("#secondanswer1")[0].checked == true){
-                $("#alert1").append("<h4 style='color: rgb(221, 6, 6);'><strong> Lo siento, la respuesta ha sido incorrecta. <a href='mathfirstclass1.html'>Repasa los conceptos</a> e intentalo de nuevo</strong><h4>");
+                $("#alert1").append("<h4 style='color: rgb(221, 6, 6);'><strong> Lo siento, la respuesta ha sido incorrecta. <a href='maththirdclass1.html'>Repasa los conceptos</a> e intentalo de nuevo</strong><h4>");
             } else if($("#thirdanswer1")[0].checked == true){
                 $("#alert1").append("<h4 style='color: #00BB0E;'><strong>La respuesta es correcta!</strong></h4>");
             } else if($("#fourthanswer1")[0].checked == true){
-                $("#alert1").append("<h4 style='color: rgb(221, 6, 6);'><strong> Lo siento, la respuesta ha sido incorrecta. <a href='mathfirstclass1.html'>Repasa los conceptos</a> e intentalo de nuevo</strong></h4>");
+                $("#alert1").append("<h4 style='color: rgb(221, 6, 6);'><strong> Lo siento, la respuesta ha sido incorrecta. <a href='maththirdclass1.html'>Repasa los conceptos</a> e intentalo de nuevo</strong></h4>");
             } else{
                 $("#alert1").append("<h4 style='color: rgb(221, 6, 6); text-align: center;'><strong>Porfavor selecciona una respuesta!</strong></h4>");
            }
@@ -48,21 +48,21 @@ jQuery(document).ready(function($){
            if($("#firstanswer2")[0].checked == true){
                 $("#alert2").append("<h4 style='color: #00BB0E;'><strong>La respuesta es correcta!</strong></h4>");
             } else if($("#secondanswer2")[0].checked == true){
-                $("#alert2").append("<h4 style='color: rgb(221, 6, 6);'><strong> Lo siento, la respuesta ha sido incorrecta. <a href='mathfirstclass1.html'>Repasa los conceptos</a> e intentalo de nuevo</strong><h4>");
+                $("#alert2").append("<h4 style='color: rgb(221, 6, 6);'><strong> Lo siento, la respuesta ha sido incorrecta. <a href='maththirdclass1.html'>Repasa los conceptos</a> e intentalo de nuevo</strong><h4>");
             } else if($("#thirdanswer2")[0].checked == true){
-                $("#alert2").append("<h4 style='color: rgb(221, 6, 6);'><strong> Lo siento, la respuesta ha sido incorrecta. <a href='mathfirstclass1.html'>Repasa los conceptos</a> e intentalo de nuevo</strong><h4>");
+                $("#alert2").append("<h4 style='color: rgb(221, 6, 6);'><strong> Lo siento, la respuesta ha sido incorrecta. <a href='maththirdclass1.html'>Repasa los conceptos</a> e intentalo de nuevo</strong><h4>");
             } else if($("#fourthanswer2")[0].checked == true){
-                $("#alert2").append("<h4 style='color: rgb(221, 6, 6);'><strong> Lo siento, la respuesta ha sido incorrecta. <a href='mathfirstclass1.html'>Repasa los conceptos</a> e intentalo de nuevo</strong></h4>");
+                $("#alert2").append("<h4 style='color: rgb(221, 6, 6);'><strong> Lo siento, la respuesta ha sido incorrecta. <a href='maththirdclass1.html'>Repasa los conceptos</a> e intentalo de nuevo</strong></h4>");
             } else{
                 $("#alert2").append("<h4 style='color: rgb(221, 6, 6); text-align: center;'><strong>Porfavor selecciona una respuesta!</strong></h4>");
             }
             
             if($("#firstanswer3")[0].checked == true){
-                $("#alert3").append("<h4 style='color: rgb(221, 6, 6);'><strong> Lo siento, la respuesta ha sido incorrecta. <a href='mathfirstclass1.html'>Repasa los conceptos</a> e intentalo de nuevo</strong><h4>");
+                $("#alert3").append("<h4 style='color: rgb(221, 6, 6);'><strong> Lo siento, la respuesta ha sido incorrecta. <a href='maththirdclass1.html'>Repasa los conceptos</a> e intentalo de nuevo</strong><h4>");
             } else if($("#secondanswer3")[0].checked == true){
-                $("#alert3").append("<h4 style='color: rgb(221, 6, 6);'><strong> Lo siento, la respuesta ha sido incorrecta. <a href='mathfirstclass1.html'>Repasa los conceptos</a> e intentalo de nuevo</strong><h4>");
+                $("#alert3").append("<h4 style='color: rgb(221, 6, 6);'><strong> Lo siento, la respuesta ha sido incorrecta. <a href='maththirdclass1.html'>Repasa los conceptos</a> e intentalo de nuevo</strong><h4>");
             } else if($("#thirdanswer3")[0].checked == true){
-                $("#alert3").append("<h4 style='color: rgb(221, 6, 6);'><strong> Lo siento, la respuesta ha sido incorrecta. <a href='mathfirstclass1.html'>Repasa los conceptos</a> e intentalo de nuevo</strong><h4>");
+                $("#alert3").append("<h4 style='color: rgb(221, 6, 6);'><strong> Lo siento, la respuesta ha sido incorrecta. <a href='maththirdclass1.html'>Repasa los conceptos</a> e intentalo de nuevo</strong><h4>");
             } else if($("#fourthanswer3")[0].checked == true){
                 $("#alert3").append("<h4 style='color: #00BB0E;'><strong>La respuesta es correcta!</strong></h4>");
             } else{
